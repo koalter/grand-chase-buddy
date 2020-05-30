8db1ddaf6980c5a18772d1f677f865155137e629
koalter <heavens_drive090@hotmail.com>
2020-05-29 23:36:51 -0300

    Added Defense Builder and heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { DefenseCalculatorComponent } from './defense-calculator/defense-calculator.component';
 import { DefenseBuilderComponent } from './defense-builder/defense-builder.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { DefenseBuilderComponent } from './defense-builder/defense-builder.compo
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/defense-builder/defense-builder.component.ts
+++ src/app/defense-builder/defense-builder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-defense-builder',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefenseBuilderComponent implements OnInit {
 
+  heroes: Array<Hero> = HEROES;
+  teamList: Array<Hero> = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addHero(e, id): void {
+    let nullIndex: number = this.teamList.findIndex(hero => hero == null);
+    let index: number;
+
+    if (this.teamList.length >= 8 && nullIndex === -1) {
+      console.log('Array is full');
+    }
+    else {
+      if (nullIndex != -1) {
+        this.teamList.splice(nullIndex, 1, this.heroes[id]);
+        index = nullIndex;
+      }
+      else {
+        this.teamList.push(this.heroes[id]);
+        index = this.teamList.length -1;
+      }
+      let el = document.getElementById(index.toString()).children[0];
+      
+      el.innerHTML = this.heroes[id].name;
+      
+      e.target.parentElement.classList.add('disabled');
+    }
+  }
+
+  removeHero(e): void {
+    let name = e.target.innerHTML;
+    this.teamList.splice(this.teamList.findIndex(hero => hero != null && hero.name == name), 1, null);
+    e.target.innerHTML = '';
+    document.getElementById(name).classList.remove('disabled');
+  }
 }
