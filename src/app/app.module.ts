1b33a6fdbd980b82a2af82f60819c0b6e1198bef
koalter <heavens_drive090@hotmail.com>
2020-06-05 01:41:40 -0300

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DefenseCalculatorComponent } from './defense-calculator/defense-calculator.component';
 import { DefenseBuilderComponent } from './defense-builder/defense-builder.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'calculator', component: DefenseCalculatorComponent },
+  { path: 'builder', component: DefenseBuilderComponent },
+  { path: '', redirectTo: '/calculator', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -19,7 +26,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatGridListModule
+    MatGridListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
